f1bd4c65bd13b7bf7ca49fc4960b29e597874362
Juraj Smiesko <juraj.smiesko@cern.ch>
2022-11-03 15:47:28 +0100

    Set up first detector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PhoenixUIModule } from 'phoenix-ui-components';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { CldComponent } from './detectors/cld/cld.component';
+import { MainComponent } from './main/main.component';
+
+let routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'cld', component: CldComponent }
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CldComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PhoenixUIModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
