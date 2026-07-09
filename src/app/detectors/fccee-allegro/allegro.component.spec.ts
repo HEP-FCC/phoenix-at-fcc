682d60588395f1b76b67f5cd26f3b15f3fd0cc6b
pablo <74920898+apausa@users.noreply.github.com>
2026-07-09 13:35:36 +0200

    Fix tests

--- src/app/detectors/fccee-allegro/allegro.component.spec.ts
+++ src/app/detectors/fccee-allegro/allegro.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AllegroComponent } from './allegro.component';
 
@@ -8,7 +11,9 @@ describe('AllegroComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AllegroComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ AllegroComponent ],
+      providers: [ provideHttpClient(), provideHttpClientTesting() ],
     })
     .compileComponents();
 
--- src/app/detectors/fccee-idea/idea.component.spec.ts
+++ src/app/detectors/fccee-idea/idea.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { IdeaComponent } from './idea.component';
 
@@ -8,7 +11,9 @@ describe('IdeaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IdeaComponent ]
+      imports: [ RouterTestingModule ],
+      declarations: [ IdeaComponent ],
+      providers: [ provideHttpClient(), provideHttpClientTesting() ],
     })
     .compileComponents();
 
